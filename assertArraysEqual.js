8a8197de7025e4b82c6e2997e271768637c6d1cd
Jessica Seo <jessicaseo83@gmail.com>
2020-05-27 00:58:06 +0000

    lint

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,18 @@
-const trueFalseArray = function (array1, array2){
-  let resultArray =[];
-    for (let i = 0 ; i < array1.length; i ++) {
-  
-      if (array1[i] === array2[i]) {
-        resultArray.push("true");
-      } 
-      else if (array1[i] !== array2[i]) {
-        resultArray.push("false");
-      }
-    }
-    return resultArray ;
-  }
+const trueFalseArray = function(array1, array2) {
+  let resultArray = [];
+  for (let i = 0; i < array1.length; i ++) {
 
-const eqArrays = function (array1, array2) {
-  const result = trueFalseArray(array1, array2)
+    if (array1[i] === array2[i]) {
+      resultArray.push("true");
+    } else if (array1[i] !== array2[i]) {
+      resultArray.push("false");
+    }
+  }
+  return resultArray;
+};
+
+const eqArrays = function(array1, array2) {
+  const result = trueFalseArray(array1, array2);
   if (result.includes("false")) {
     return false;
   } else {
@@ -21,11 +20,11 @@ const eqArrays = function (array1, array2) {
   }
 };
 
-const assertArrayEqual = function (array1, array2) {
-  const eqArraysResult = eqArrays(array1, array2)
-    if(!eqArraysResult) {
-      console.log("Assertion Failed");
-    } else if (eqArraysResult) {
-      console.log("Assertion Passed");
-    }
-}
+const assertArraysEqual = function(array1, array2) {
+  const eqArraysResult = eqArrays(array1, array2);
+  if (!eqArraysResult) {
+    console.log("Assertion Failed");
+  } else if (eqArraysResult) {
+    console.log("Assertion Passed");
+  }
+};
--- eqArrays.js
+++ eqArrays.js
@@ -6,22 +6,21 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const trueFalseArray = function (array1, array2){
-  let resultArray =[];
-    for (let i = 0 ; i < array1.length; i ++) {
-  
-      if (array1[i] === array2[i]) {
-        resultArray.push("true");
-      } 
-      else if (array1[i] !== array2[i]) {
-        resultArray.push("false");
-      }
-    }
-    return resultArray ;
-  }
+const trueFalseArray = function(array1, array2) {
+  let resultArray = [];
+  for (let i = 0; i < array1.length; i ++) {
 
-const eqArrays = function (array1, array2) {
-  const result = trueFalseArray(array1, array2)
+    if (array1[i] === array2[i]) {
+      resultArray.push("true");
+    } else if (array1[i] !== array2[i]) {
+      resultArray.push("false");
+    }
+  }
+  return resultArray;
+};
+
+const eqArrays = function(array1, array2) {
+  const result = trueFalseArray(array1, array2);
   if (result.includes("false")) {
     return false;
   } else {
@@ -30,4 +29,5 @@ const eqArrays = function (array1, array2) {
 };
 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), true); 
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), true);
