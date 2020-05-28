613a437d0c49094ddfea2f5bd0c7434daceab114
Jessica Seo <jessicaseo83@gmail.com>
2020-05-28 00:03:31 +0000

    Implement findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  } else if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  let result = undefined;
+  for (let key of Object.keys(object)) {
+    if (object[key] === value) {
+      result = key;
+    }
+  }
+  return result;
+};
+
+// Test code
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
