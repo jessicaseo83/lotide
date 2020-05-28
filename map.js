22862598e4620e79ae1cde3c0a0ff9824af12aff
Jessica Seo <jessicaseo83@gmail.com>
2020-05-28 20:07:19 +0000

    Implement Map

---
+++ map.js
@@ -0,0 +1,44 @@
+const eqArrays = function(array1, array2) {
+  // if arrays' lengths are not same, no need to go further.
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  // 두 어레이에서 같은 인덱스의 값이 같지 않으면, no need to go further.
+  let result = true;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      result = false;
+    }
+  }
+  return result;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  const eqArraysResult = eqArrays(array1, array2);
+  if (!eqArraysResult) {
+    console.log("🛑🛑🛑Assertion Failed");
+  } else if (eqArraysResult) {
+    console.log("✅✅✅Assertion Passed");
+  }
+};
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+ }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+
+console.log(results1);
+
+
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
+
+
