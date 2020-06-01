e4409b912ba3e475aa07d7708e37d0d5af69ba8b
Jessica Seo <jessicaseo83@gmail.com>
2020-06-01 18:47:58 +0000

    Refactoring headTest with Mocha and Chai

--- middle.js
+++ middle.js
@@ -1,27 +1,3 @@
-const eqArrays = function(array1, array2) {
-  // if arrays' lengths are not same, no need to go further.
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  // 두 어레이에서 같은 인덱스의 값이 같지 않으면, no need to go further.
-  let result = true;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      result = false;
-    }
-  }
-  return result;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  const eqArraysResult = eqArrays(array1, array2);
-  if (!eqArraysResult) {
-    console.log("🛑🛑🛑Assertion Failed");
-  } else if (eqArraysResult) {
-    console.log("✅✅✅Assertion Passed");
-  }
-};
-
 const middle = function(array) {
   const returnArray = [];
   const midIndex = Math.floor(array.length/2);
@@ -35,10 +11,5 @@ const middle = function(array) {
   }
   return returnArray;
 }
-// TestCodes
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+
+module.exports = middle;
